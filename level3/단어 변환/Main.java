1ab565403c26cf2a1aaaa80977e269d80db1a1f3
0takkk <89503136+0takkk@users.noreply.github.com>
2022-03-02 18:41:16 +0900

    Create Main.java

---
+++ level3/단어 변환/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(solution("hit", "cog", new String[] {"hot", "dot", "dog", "lot", "log", "cog"}));
+        System.out.println(solution("hit", "hot", new String[] {"hot", "dot", "dog", "lot", "log"}));
+//        int[] tmp = solution(new String[] {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"});
+//        for (int i : tmp) {
+//            System.out.print(i + " ");
+//        }
+    }
+
+    public static int solution(String begin, String target, String[] words) {
+        int answer = 0;
+
+        Queue<String> q = new LinkedList<>();
+        q.offer(begin);
+        boolean[] visited = new boolean[words.length];
+        boolean flag = false;
+
+        while(!q.isEmpty()){
+            answer++;
+
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                String s = q.poll();
+
+                if (s.equals(target)) {
+                    flag = true;
+                    break;
+                }
+
+                for(int j = 0; j < words.length; j++){
+                    int count = 0;
+
+                    if(!visited[j]){
+                        for(int k = 0; k < s.length(); k++){
+                            if(s.charAt(k) != words[j].charAt(k)){
+                                count++;
+                            }
+                            if(count >= 2) break;
+                        }
+                    }
+
+                    if(count == 1){
+                        q.offer(words[j]);
+                        System.out.println(words[j]);
+                        System.out.println(answer);
+                        visited[j] = true;
+                    }
+
+                }
+            }
+
+            if(flag) break;
+        }
+
+        if(flag) return answer-1;
+        else return 0;
+    }
+
+}
