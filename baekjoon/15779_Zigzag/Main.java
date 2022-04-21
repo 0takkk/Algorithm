b3585213e22d64fc87c617928c65bcb6949bfe05
0takkk <ss0takk@gmail.com>
2022-04-21 13:48:45 +0900

    Update Main.java

--- baekjoon/15779_Zigzag/Main.java
+++ baekjoon/15779_Zigzag/Main.java
@@ -8,24 +8,24 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
 
         int[] zigzag = new int[n];
-        for(int i = 2; i < n; i++){
-            int a = arr[i];
-            int b = arr[i-1];
-            int c = arr[i-2];
-            zigzag[i] = ((a >= b && b >= c) || (a <= b && b <= c)) ? 0 : (zigzag[i-1] == 0 ? 1 : zigzag[i-1]+1);
-        }
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
         int max = 0;
-        for(int i = 0; i < n; i++){
+
+        for(int i = 2; i < n; i++){
+            int c = Integer.parseInt(st.nextToken());
+            zigzag[i] = ((a <= b && b <= c) || (a >= b && b >= c)) ? 0 : zigzag[i-1] + 1;
+            a = b;
+            b = c;
+
             max = Math.max(max, zigzag[i]);
         }
 
         System.out.println(max+2);
     }
+
 }
