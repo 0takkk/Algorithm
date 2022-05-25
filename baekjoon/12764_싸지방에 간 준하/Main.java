91b39ee3278a998064f539092cd5f31a51e2b1eb
0takkk <ss0takk@gmail.com>
2022-05-25 16:18:07 +0900

    Update Main.java

--- baekjoon/12764_싸지방에 간 준하/Main.java
+++ baekjoon/12764_싸지방에 간 준하/Main.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class Main {
 
     public static class Node implements Comparable<Node>{
-        int num, end;
+        int num;
+        int end;
 
         public Node(int num, int end) {
             this.num = num;
@@ -13,7 +14,6 @@ public class Main {
 
         @Override
         public int compareTo(Node o) {
-            if(this.end == o.end) return this.num - o.num;
             return this.end - o.end;
         }
     }
@@ -23,9 +23,9 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n+1][2];
+        int[][] arr = new int[n][2];
 
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
@@ -34,51 +34,46 @@ public class Main {
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if(o1[0] == o2[0]) return o1[1] - o2[1];
                 return o1[0] - o2[0];
             }
         });
 
+        PriorityQueue<Node> seat = new PriorityQueue<>();
+        PriorityQueue<Integer> vacant = new PriorityQueue<>();
+        int[] count = new int[100001];
+
+        seat.offer(new Node(1, arr[0][1]));
+        count[1] = 1;
+
         int idx = 1;
-        int[] seat = new int[100001];
-        PriorityQueue<Integer> space = new PriorityQueue<>();
-        PriorityQueue<Node> pq = new PriorityQueue<>();
 
-        pq.offer(new Node(1, arr[1][1]));
-        seat[1] = 1;
-
-        for(int i = 2; i <= n; i++){
+        for(int i = 1; i < n; i++){
             int start = arr[i][0];
             int end = arr[i][1];
 
-            if(!pq.isEmpty() && pq.peek().end <= start){
-                while(!pq.isEmpty() && pq.peek().end <= start){
-                    space.offer(pq.poll().num);
-                }
-                int num = space.poll();
-                seat[num]++;
-                pq.offer(new Node(num, end));
+            while(!seat.isEmpty() && seat.peek().end <= start){
+                int num = seat.poll().num;
+                vacant.offer(num);
+            }
+
+            if(vacant.isEmpty()){
+                seat.offer(new Node(++idx, end));
+                count[idx]++;
             }
             else{
-                int num;
-
-                if(space.isEmpty()) num = ++idx;
-                else num = space.poll();
-
-                pq.offer(new Node(num, end));
-                seat[num]++;
+                int num = vacant.poll();
+                seat.offer(new Node(num, end));
+                count[num]++;
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        int size = 0;
-        for(int i = 1; i < seat.length; i++){
-            if(seat[i] == 0) break;
-            size++;
-            sb.append(seat[i] + " ");
+        sb.append(idx+"\n");
+        for(int i = 1; i < count.length; i++){
+            if(count[i] == 0 ) break;
+            sb.append(count[i] + " ");
         }
 
-        System.out.println(size);
         System.out.println(sb.toString());
     }
 
