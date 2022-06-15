eb0b37a2ca437ff658d7b99ede25bdbc0b791325
0takkk <ss0takk@gmail.com>
2022-06-15 11:03:32 +0900

    Update Main.java

--- baekjoon/9375_패션왕 신해빈/Main.java
+++ baekjoon/9375_패션왕 신해빈/Main.java
@@ -3,8 +3,6 @@ import java.util.*;
 
 public class Main {
 
-    public static HashMap<String, Integer> map;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
