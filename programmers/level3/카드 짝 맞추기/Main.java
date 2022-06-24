49bca3870fc31a85b46d6abd57c49d7b470f39b2
0takkk <ss0takk@gmail.com>
2022-06-24 19:11:41 +0900

    Update Main.java

--- programmers/level3/카드 짝 맞추기/Main.java
+++ programmers/level3/카드 짝 맞추기/Main.java
@@ -2,21 +2,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(solution(new int[][]{{1,0,0,3},{2,0,0,0},{0,0,0,2},{3,0,1,0}}, 1, 0));
-    }
-
-    public static class Cursor{
-        int x;
-        int y;
-
-        public Cursor(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+        System.out.println(solution(new int[][]{{1,0,0,3}, {2,0,0,0}, {0,0,0,2}, {3,0,1,0}}, 1, 0));
     }
 
     public static class Pos{
-        int x, y, move;
+        int x, y;
+        int move;
+
+        public Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
 
         public Pos(int x, int y, int move) {
             this.x = x;
@@ -25,54 +21,52 @@ public class Main {
         }
     }
 
-    public static int cardCnt;
     public static int[] cardNum;
+    public static int cardCnt;
     public static ArrayList<String> orders;
+    public static int[][] copy;
 
-    public static int[][] copyBoard;
     public static int[] dx = {0, 0, 1, -1};
     public static int[] dy = {1, -1, 0, 0};
 
+    public static Pos cursor;
+
     public static int solution(int[][] board, int r, int c) {
         cardNum = new int[7];
-
-        for(int i=0; i<4; i++) {
-            for(int j=0; j<4; j++) {
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
                 int num = board[i][j];
 
-                if(num != 0) {
-                    cardNum[num]++;
-                    if(cardNum[num] == 2) cardCnt++;
-                }
+                if(num == 0) continue;
+
+                if(++cardNum[num] == 2) cardCnt++;
             }
         }
 
         orders = new ArrayList<>();
         makeOrders("", 0);
 
-//        System.out.println(orders);
-
         int min = Integer.MAX_VALUE;
-        for(String order : orders){
+        for (String order : orders) {
             int move = 0;
-            Cursor cursor = new Cursor(r, c);
+            cursor = new Pos(r, c);
+            copy = new int[4][4];
 
-            copyBoard = new int[4][4];
             for(int i = 0; i < 4; i++){
                 for(int j = 0; j < 4; j++){
-                    copyBoard[i][j] = board[i][j];
+                    copy[i][j] = board[i][j];
                 }
             }
 
             for(int i = 0; i < order.length(); i++){
-                int target = order.charAt(i)-'0';
+                int target = order.charAt(i) - '0';
 
                 move += search(cursor, target);
-                copyBoard[cursor.x][cursor.y] = 0;
+                copy[cursor.x][cursor.y] = 0;
                 move += 1;
 
                 move += search(cursor, target);
-                copyBoard[cursor.x][cursor.y] = 0;
+                copy[cursor.x][cursor.y] = 0;
                 move += 1;
             }
 
@@ -88,19 +82,19 @@ public class Main {
             return;
         }
 
-        for(int i = 1; i < cardNum.length; i++){
+        for(int i = 1; i < 7; i++){
             if(cardNum[i] == 2){
                 if(!str.contains(i+"")){
-                    makeOrders(str + i, cnt+1);
+                    makeOrders(str+i, cnt+1);
                 }
             }
         }
     }
 
-    public static int search(Cursor cursor, int target){
-        Queue<Pos> q = new LinkedList<>();
+    public static int search(Pos cursor, int target){
         boolean[][] visited = new boolean[4][4];
-        q.offer(new Pos(cursor.x, cursor.y, 0));
+        Queue<Pos> q = new LinkedList<>();
+        q.offer(cursor);
         visited[cursor.x][cursor.y] = true;
 
         while(!q.isEmpty()){
@@ -109,18 +103,18 @@ public class Main {
             int y = p.y;
             int move = p.move;
 
-            if(copyBoard[x][y] == target){
+            if(copy[x][y] == target){
                 cursor.x = x;
                 cursor.y = y;
                 return move;
             }
 
-            // 한칸 방향키
+            // 한칸 움직임
             for(int i = 0; i < 4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if(nx < 0 || nx >= 4 || ny < 0 || ny >= 4) continue;
+                if(!isRange(nx, ny)) continue;
 
                 if(!visited[nx][ny]){
                     visited[nx][ny] = true;
@@ -128,13 +122,12 @@ public class Main {
                 }
             }
 
-            // ctrl 방향키
+            // ctrl
             for(int i = 0; i < 4; i++){
                 Pos np = check(x, y, i);
                 int nx = np.x;
                 int ny = np.y;
 
-                // 움직이지 않았으면 continue
                 if(nx == x && ny == y) continue;
 
                 if(!visited[nx][ny]){
@@ -152,13 +145,18 @@ public class Main {
         x += dx[dir];
         y += dy[dir];
 
-        while(x >= 0 && x < 4 && y >= 0 && y < 4){
-            if(copyBoard[x][y] != 0) return new Pos(x, y, 0);
+        while(isRange(x, y)){
+            if(copy[x][y] != 0) return new Pos(x, y);
 
             x += dx[dir];
             y += dy[dir];
         }
 
-        return new Pos(x - dx[dir], y - dy[dir], 0);
+        return new Pos(x - dx[dir], y - dy[dir]);
     }
+
+    public static boolean isRange(int x, int y){
+        return x >= 0 && x < 4 && y >= 0 && y < 4;
+    }
+
 }
