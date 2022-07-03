28b869ff9f40cf860862f4ce1b01a35e91dbfd08
0takkk <ss0takk@gmail.com>
2022-07-03 13:05:12 +0900

    Update Main.java

--- baekjoon/14699_관악산 등산/Main.java
+++ baekjoon/14699_관악산 등산/Main.java
@@ -4,9 +4,8 @@ import java.util.*;
 public class Main {
 
     public static int n;
-    public static int[] height;
-    public static HashSet<Integer>[] list;
-    public static int[] count;
+    public static int[] height, dp;
+    public static HashSet<Integer>[] graph;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,13 +15,16 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
+        dp = new int[n+1];
         height = new int[n+1];
-        list = new HashSet[n+1];
-
         st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
             height[i] = Integer.parseInt(st.nextToken());
-            list[i] = new HashSet<>();
+        }
+
+        graph = new HashSet[n+1];
+        for(int i = 1; i <= n; i++){
+            graph[i] = new HashSet<>();
         }
 
         while(m-->0){
@@ -30,26 +32,26 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if(height[a] > height[b]) list[b].add(a);
-            else list[a].add(b);
+            if(height[a] > height[b]) graph[b].add(a);
+            else graph[a].add(b);
         }
 
-        count = new int[n+1];
-
+        StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= n; i++){
-            System.out.println(dfs(i));
+            sb.append(dfs(i) + "\n");
         }
+        System.out.println(sb.toString());
     }
 
     public static int dfs(int idx){
-        if(count[idx] != 0) return count[idx];
+        if(dp[idx] != 0) return dp[idx];
 
-        count[idx] = 1;
-
-        for(int next : list[idx]){
-            count[idx] = Math.max(count[idx], dfs(next)+1);
+        dp[idx] = 1;
+        for (int next : graph[idx]) {
+            dp[idx] = Math.max(dp[idx], dfs(next)+1);
         }
-        return count[idx];
+
+        return dp[idx];
     }
 
 }
