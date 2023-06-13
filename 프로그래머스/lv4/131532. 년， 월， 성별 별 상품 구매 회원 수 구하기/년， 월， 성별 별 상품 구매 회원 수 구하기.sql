70c72494d6dca50696c581afb0250d1aa4a47497
0takkk <ss0takk@gmail.com>
2023-06-13 21:15:12 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/lv4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -6,13 +6,5 @@ SELECT substring(o.sales_date, 1, 4) as year,
 from online_sale o 
 join user_info u on o.user_id = u.user_id
 where u.gender is not null
-# group by o.sales_date
 group by substring(o.sales_date, 1, 7), u.gender
 order by year, month, u.gender
-
-# SELECT *
-# from online_sale o 
-# join user_info u on o.user_id = u.user_id
-# where u.gender is not null
-# # group by o.sales_date
-# group by substring(o.sales_date, 1, 4), substring(o.sales_date, 6, 2), u.gender
