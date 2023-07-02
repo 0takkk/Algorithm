1cbeb8b5412abd19a9860d8bac363ce439295d4f
0takkk <ss0takk@gmail.com>
2023-07-02 17:56:31 +0900

    [Silver I] Title: 지름길, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -3,56 +3,39 @@ import java.util.*;
 
 public class Main {
 
-    public static int d, ans;
-    public static int[][] shortcut;
-    public static boolean[] hasShortcut;
-    public static HashSet<Integer>[] endPoint;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-        shortcut = new int[d+1][d+1];
-        ans = Integer.MAX_VALUE;
-        hasShortcut = new boolean[d+1];
-        endPoint = new HashSet[d+1];
+        int d = Integer.parseInt(st.nextToken());
 
-        while(n-->0){
+        int[] dist = new int[d+1];
+        int[][] shortcut = new int[n][3];
+
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int time = Integer.parseInt(st.nextToken());
 
-            if(start > d || end > d || end - start + 1 <= time) continue;
-
-            shortcut[start][end] = shortcut[start][end] == 0 ? time : Math.min(shortcut[start][end], time);
-            hasShortcut[start] = true;
-
-            if(endPoint[start] == null) endPoint[start] = new HashSet<>();
-            endPoint[start].add(end);
+            shortcut[i] = new int[]{start, end, time};
         }
 
-        dfs(0, 0);
-        System.out.println(ans);
-    }
+        for(int i = 1; i <= d; i++){
+            int cost = dist[i-1] + 1;
 
-    public static void dfs(int now, int time){
-        if(time >= ans) return;
-
-        if(now == d){
-            ans = time;
-            return;
-        }
-
-        dfs(now+1, time+1);
-        if(hasShortcut[now]){
-            for (int next : endPoint[now]) {
-                dfs(next, time+shortcut[now][next]);
+            for(int j = 0; j < n; j++){
+                if(shortcut[j][1] == i){
+                    cost = Math.min(cost, shortcut[j][2] + dist[shortcut[j][0]]);
+                }
             }
+
+            dist[i] = cost;
         }
+
+        System.out.println(dist[d]);
     }
 
 }
