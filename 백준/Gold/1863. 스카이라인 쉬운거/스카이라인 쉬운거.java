f3c17470fc550ad3409140368b8106b2c56038a3
0takkk <ss0takk@gmail.com>
2023-07-10 14:26:21 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 232 ms, Memory: 19968 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -8,55 +8,26 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] x = new int[n];
-        int[] y = new int[n];
+        int[] skyline = new int[n+1];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            x[i] = Integer.parseInt(st.nextToken());
-            y[i] = Integer.parseInt(st.nextToken());
+            Integer.parseInt(st.nextToken());
+            skyline[i] = Integer.parseInt(st.nextToken());
         }
-
-        int size = x[n - 1] + 2;
-        int[] skyline = new int[size];
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = x[i]; j < x[i + 1]; j++) {
-                skyline[j] = y[i];
-            }
-        }
-
-        skyline[x[n - 1]] = y[n - 1];
 
         int ans = 0;
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = 1; i < size; i++) {
-            int height = skyline[i];
-
-            if (!stack.isEmpty()) {
-                if(stack.peek() == height) continue;
-                else if(stack.peek() < height){
-                    stack.push(height);
-                }
-                else{
-                    while(!stack.isEmpty() && stack.peek() > height){
-                        stack.pop();
-                        ans++;
-                    }
-
-                    if(height != 0 && (stack.isEmpty() || stack.peek() != height)){
-                        stack.push(height);
-                    }
-                }
+        for(int i = 0; i <= n; i++){
+            while(!stack.isEmpty() && stack.peek() > skyline[i]){
+                ans++;
+                stack.pop();
             }
-            else if(height != 0){
-                stack.push(height);
-            }
-        }
 
-        while(!stack.isEmpty()){
-            stack.pop();
-            ans++;
+            if(stack.isEmpty() || stack.peek() != skyline[i]){
+                stack.push(skyline[i]);
+            }
         }
 
         System.out.println(ans);
