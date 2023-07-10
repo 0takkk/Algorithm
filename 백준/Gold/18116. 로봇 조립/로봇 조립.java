59c5e6694e07eabe5909a1fa796a95018e1cda2d
0takkk <ss0takk@gmail.com>
2023-07-10 12:00:19 +0900

    [Gold IV] Title: 로봇 조립, Time: 2664 ms, Memory: 521040 KB -BaekjoonHub

---
+++ 백준/Gold/18116. 로봇 조립/로봇 조립.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static final int size = 1000001;
+    public static int[] parent;
+    public static HashSet<Integer>[] set;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        parent = new int[size];
+        set = new HashSet[size];
+        for(int i = 1; i < size; i++){
+            parent[i] = i;
+            set[i] = new HashSet<>();
+            set[i].add(i);
+        }
+
+        while(n-->0){
+            st = new StringTokenizer(br.readLine());
+            char comd = st.nextToken().charAt(0);
+
+            if(comd == 'I'){
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                union(a, b);
+            }
+            else{
+                int c = Integer.parseInt(st.nextToken());
+                sb.append(set[find(c)].size()).append("\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static int find(int x){
+        if(x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int x, int y){
+        x = find(x);
+        y = find(y);
+
+        if(x == y) return;
+
+        if(x > y){
+            int tmp = x;
+            x = y;
+            y = tmp;
+        }
+
+        parent[y] = x;
+        set[x].addAll(set[y]);
+    }
+
+}
