90a5e29ca4811501dd655f51b81bc51edefeaa29
0takkk <ss0takk@gmail.com>
2023-07-24 14:32:28 +0900

    [Gold V] Title: 조 짜기, Time: 164 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Gold/2229. 조 짜기/조 짜기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n+1];
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1];
+            int max = 0;
+            int min = Integer.MAX_VALUE;
+
+            for(int j = i; j > 0; j--){
+                max = Math.max(max, arr[j]);
+                min = Math.min(min, arr[j]);
+
+                dp[i] = Math.max(dp[i], dp[j-1]+max-min);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
