712cd2f61cca39e017670e0327cbead7cfd9e75c
0takkk <ss0takk@gmail.com>
2023-07-24 14:46:57 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 136 ms, Memory: 18016 KB -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        if(n > 0) dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
