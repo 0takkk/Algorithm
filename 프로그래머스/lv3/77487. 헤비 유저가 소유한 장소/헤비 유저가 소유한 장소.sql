f2fcc975e30558d65825e94553386a455a90b99b
0takkk <ss0takk@gmail.com>
2023-08-08 21:12:18 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,14 @@
+select host_id 
+from places 
+group by host_id
+having count(id) >= 2;
+
+select *
+from places
+where host_id in (
+    select host_id
+    from places
+    group by host_id
+    having count(id) >= 2
+)
+order by id;
