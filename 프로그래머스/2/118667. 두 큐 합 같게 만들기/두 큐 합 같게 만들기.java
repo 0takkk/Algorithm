01599add8e2475c9d29912ee978175bcdbdde6d6
0takkk <ss0takk@gmail.com>
2023-11-22 16:05:32 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 7.30 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,34 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] queue1, int[] queue2) {
-        int answer = 0;
+    public static int solution(int[] queue1, int[] queue2) {
+        int ans = 0;
         long sum1 = 0, sum2 = 0;
         int len = queue1.length;
-        
-        for(int q : queue1) {
-            sum1 += q;
-        }
-        
-        for(int q : queue2) {
-            sum2 += q;
-        }
-        
+
         int[] q1 = new int[2*len];
+        int[] q2 = new int[2*len];
+
         for(int i = 0; i < len; i++) {
             q1[i] = queue1[i];
-            q1[i+len] = queue2[i];
-        }
-        
-        int[] q2 = new int[2*len];
-        for(int i = 0; i < len; i++) {
+            q1[len+i] = queue2[i];
+            sum1 += q1[i];
+
             q2[i] = queue2[i];
-            q2[i+len] = queue1[i];
+            q2[len+i] = queue1[i];
+            sum2 += q2[i];
         }
-        
+
         int idx1 = 0, idx2 = 0;
+
         boolean flag = false;
-        
         while(idx1 < 2*len && idx2 < 2*len) {
             if(sum1 == sum2) {
                 flag = true;
@@ -44,10 +37,10 @@ class Solution {
                 sum1 += num;
                 sum2 -= num;
             }
-            answer++;
+            ans++;
         }
-        
-        if(!flag) return -1;
-        return answer;
+
+        if(!flag) ans = -1;
+        return ans;
     }
 }
