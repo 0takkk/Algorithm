76588ec2dcb29e839cd5cf1c7115ce3307814a89
0takkk <ss0takk@gmail.com>
2023-11-22 21:47:38 +0900

    [level 3] Title: 등산코스 정하기, Time: 158.60 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -16,76 +16,74 @@ class Solution {
         }
     }
 
-    public static int len, answer[];
     public static ArrayList<Node>[] graph;
-    public static HashSet<Integer> gateSet, summitSet;
+    public static int[] intensities;
+    public static boolean[] isGate, isSummit;
 
     public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        len = n;
-        graph = new ArrayList[len+1];
-        gateSet = new HashSet<>();
-        summitSet = new HashSet<>();
+        graph = new ArrayList[n+1];
+        intensities = new int[n+1];
 
-        for(int i = 1; i <= len; i++) {
+        for(int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
+            intensities[i] = Integer.MAX_VALUE;
         }
 
         for (int[] path : paths) {
-            int a = path[0];
-            int b = path[1];
-            int c = path[2];
-
-            graph[a].add(new Node(b, c));
-            graph[b].add(new Node(a, c));
+            int from = path[0];
+            int to = path[1];
+            int cost = path[2];
+            graph[from].add(new Node(to, cost));
+            graph[to].add(new Node(from, cost));
         }
+
+        isGate = new boolean[n+1];
+        isSummit = new boolean[n+1];
 
         for (int gate : gates) {
-            gateSet.add(gate);
+            isGate[gate] = true;
         }
-
         for (int summit : summits) {
-            summitSet.add(summit);
+            isSummit[summit] = true;
         }
 
-        answer = new int[] {0, Integer.MAX_VALUE};
         for (int gate : gates) {
-            bfs(gate);
+            dijkstra(gate);
         }
 
+        int[] answer = new int[] {0, Integer.MAX_VALUE};
+        for (int summit : summits) {
+            int intensity = intensities[summit];
+            if(intensity == answer[1]) {
+                answer[0] = Math.min(answer[0], summit);
+            }
+            else if(intensity < answer[1]) {
+                answer[0] = summit;
+                answer[1] = intensity;
+            }
+        }
+        
         return answer;
     }
 
-    public static void bfs(int start) {
-        int[] visited = new int[len+1];
+    public static void dijkstra(int start) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(start, 0));
-        Arrays.fill(visited, Integer.MAX_VALUE);
-        visited[start] = 0;
+        intensities[start] = 0;
 
         while(!pq.isEmpty()) {
             Node now = pq.poll();
 
-            if(now.cost > answer[1]) return;
-
-            if(summitSet.contains(now.x)) {
-                if(now.cost == answer[1]) {
-                    answer[0] = Math.min(answer[0], now.x);
-                }
-                else if(now.cost < answer[1]) {
-                    answer[0] = now.x;
-                    answer[1] = now.cost;
-                }
-                continue;
-            }
+            if(now.cost > intensities[now.x]) continue;
+            if(isSummit[now.x]) continue;
 
             for (Node next : graph[now.x]) {
-                if(visited[next.x] > next.cost && !gateSet.contains(next.x)) {
-                    visited[next.x] = next.cost;
-                    pq.offer(new Node(next.x, Math.max(now.cost, next.cost)));
+                if(intensities[next.x] > Math.max(now.cost, next.cost) && !isGate[next.x]) {
+                    intensities[next.x] = Math.max(now.cost, next.cost);
+                    pq.offer(new Node(next.x, intensities[next.x]));
                 }
             }
         }
     }
-
     
 }
