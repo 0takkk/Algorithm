de231f2abfbc81809d53d82884bbf8e802c41e77
0takkk <ss0takk@gmail.com>
2023-12-13 17:13:24 +0900

    [Gold IV] Title: 작업, Time: 500 ms, Memory: 59836 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.java
+++ 백준/Gold/2056. 작업/작업.java
@@ -9,46 +9,24 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] times = new int[n+1];
-        int[] workTimes = new int[n+1];
-        int[] degree = new int[n+1];
-        ArrayList<Integer>[] graph = new ArrayList[n+1];
-
-        Queue<Integer> q = new ArrayDeque<>();
 
         for(int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             times[i] = Integer.parseInt(st.nextToken());
 
-            graph[i] = new ArrayList<>();
             int k = Integer.parseInt(st.nextToken());
-            if(k == 0) {
-                workTimes[i] = times[i];
-                q.offer(i);
-            }
+            int max = 0;
             while(k-->0) {
                 int work = Integer.parseInt(st.nextToken());
-                degree[i]++;
-                graph[work].add(i);
+                max = Math.max(max, times[work]);
             }
-        }
 
-        while(!q.isEmpty()) {
-            int now = q.poll();
-
-            for (int next : graph[now]) {
-                degree[next]--;
-                workTimes[next] = Math.max(workTimes[next], workTimes[now]);
-
-                if(degree[next] == 0) {
-                    q.offer(next);
-                    workTimes[next] += times[next];
-                }
-            }
+            times[i] += max;
         }
 
         int ans = 0;
-        for (int workTime : workTimes) {
-            ans = Math.max(ans, workTime);
+        for (int time : times) {
+            ans = Math.max(ans, time);
         }
 
         System.out.println(ans);
