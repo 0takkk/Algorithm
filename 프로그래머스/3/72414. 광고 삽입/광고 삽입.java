6913f407beb0478df41dc9bf968f73f2c80d2820
0takkk <ss0takk@gmail.com>
2024-03-21 21:56:13 +0900

    [level 3] Title: 광고 삽입, Time: 4.77 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -2,36 +2,36 @@ class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
         String answer = "";
         
-        int playTimes = timeToSecond(play_time);
-        int[] watch = new int[playTimes];
+        int playTime = timeToSecond(play_time);
+        int advTime = timeToSecond(adv_time);
+        long[] watch = new long[playTime+1];
         
         for(String log : logs) {
             String[] times = log.split("-");
             int startTime = timeToSecond(times[0]);
             int endTime = timeToSecond(times[1]);
-            
-            for(int i = startTime; i < endTime; i++) {
-                watch[i]++;
-            }
+            watch[startTime]++;
+            watch[endTime]--;
         }
         
+        for(int i = 0; i < playTime; i++) {
+            watch[i+1] += watch[i];
+        }
+        
+        int startTime = 0;
         int ans = 0;
-        long watchTimes = 0;
-        long maxWatch = 0;
-        int advTime = timeToSecond(adv_time);
+        long watchTime = 0;
+        long maxWatchTime = 0;
         
-        for(int i = 0; i < advTime; i++) {
-            watchTimes += watch[i];
-        }
-        maxWatch = watchTimes;
-
-        for(int i = 1; i < playTimes-advTime+1; i++) {
-            watchTimes -= watch[i-1];
-            watchTimes += watch[i+advTime-1];
-            if(maxWatch < watchTimes) {
-                maxWatch = watchTimes;
-                ans = i;
+        while(startTime + advTime <= playTime) {
+            if(maxWatchTime < watchTime) {
+                maxWatchTime = watchTime;
+                ans = startTime;
             }
+            
+            watchTime -= watch[startTime];
+            watchTime += watch[startTime+advTime];
+            startTime++;
         }
 
         return String.format("%02d:%02d:%02d", ans/3600, (ans/60)%60, ans%60);
@@ -45,36 +45,4 @@ class Solution {
         return h+m+s;
     }
     
-    public String secondToTime(int second) {
-        int h = second / 3600;
-        second -= h * 3600;
-        
-        int m = second / 60;
-        second -= m * 60;
-        
-        int s = second % 60;
-        
-        StringBuilder sb = new StringBuilder();
-        if(h >= 10) {
-            sb.append(h);
-        } else {
-            sb.append("0").append(h);
-        }
-        sb.append(":");
-        
-        if(m >= 10) {
-            sb.append(m);
-        } else {
-            sb.append("0").append(m);
-        }
-        sb.append(":");
-        
-        if(s >= 10) {
-            sb.append(s);
-        } else {
-            sb.append("0").append(s);
-        }
-        
-        return sb.toString();
-    }
 }
