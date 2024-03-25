813a57a2c576b0135431b944b97dc7bf45edf766
0takkk <ss0takk@gmail.com>
2024-03-25 14:38:12 +0900

    [Gold III] Title: 안정적인 네트워크, Time: 1012 ms, Memory: 101604 KB -BaekjoonHub

--- 백준/Gold/2406. 안정적인 네트워크/안정적인 네트워크.java
+++ 백준/Gold/2406. 안정적인 네트워크/안정적인 네트워크.java
@@ -17,7 +17,6 @@ public class Main {
             return this.cost - o.cost;
         }
     }
-
     public static int[] parent;
 
     public static void main(String[] args) throws IOException {
@@ -38,47 +37,44 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if(find(a) != find(b)) {
-                union(a, b);
-            }
+            union(a, b);
         }
 
-        int[][] cost = new int[n+1][n+1];
+        int[][] arr = new int[n+1][n+1];
         for(int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 1; j <= n; j++) {
-                cost[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         PriorityQueue<Node> pq = new PriorityQueue<>();
         for(int i = 2; i <= n; i++) {
             for(int j = i+1; j <= n; j++) {
-                pq.offer(new Node(i, j, cost[i][j]));
+                pq.offer(new Node(i, j, arr[i][j]));
             }
         }
 
-        int min = 0;
+        int totalCost = 0;
         int count = 0;
-        ArrayList<Node> list = new ArrayList<>();
-
+        ArrayList<Node> result = new ArrayList<>();
         while(!pq.isEmpty()) {
             Node node = pq.poll();
+            int from = node.from;
+            int to = node.to;
+            int cost = node.cost;
 
-            int x = find(node.from);
-            int y = find(node.to);
-
-            if(x == y) continue;
-
-            parent[y] = x;
-            min += node.cost;
-            count++;
-            list.add(node);
+            if(find(from) != find(to)) {
+                union(from, to);
+                totalCost += cost;
+                count++;
+                result.add(node);
+            }
         }
 
-        System.out.println(min + " " + count);
         StringBuilder sb = new StringBuilder();
-        for (Node node : list) {
+        sb.append(totalCost).append(" ").append(count).append("\n");
+        for (Node node : result) {
             sb.append(node.from).append(" ").append(node.to).append("\n");
         }
         System.out.println(sb.toString());
@@ -93,7 +89,9 @@ public class Main {
         x = find(x);
         y = find(y);
 
-        parent[y] = x;
+        if(x != y) {
+            parent[y] = x;
+        }
     }
 
 }
