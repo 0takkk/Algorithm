fc7ad7c4c5d678fb2667b99edb537e7de4e3606f
0takkk <ss0takk@gmail.com>
2024-04-06 15:56:01 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 256 ms, Memory: 25240 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-    public static class Pos{
+    public static class Pos {
         int x, y;
 
         public Pos(int x, int y) {
@@ -23,51 +23,50 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        while(t-->0){
+        while(t-->0) {
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
-
             map = new char[n][m];
-            for(int i = 0; i < n; i++){
+            visited = new boolean[n][m];
+
+            for(int i = 0; i < n; i++) {
                 map[i] = br.readLine().toCharArray();
             }
 
-            visited = new boolean[n][m];
-            int cnt = 0;
-
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(map[i][j] == '#' && !visited[i][j]){
-                        cnt += bfs(i, j);
+            int ans = 0;
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < m; j++) {
+                    if(isSheep(i, j) && !visited[i][j]) {
+                        ans += bfs(new Pos(i, j));
                     }
                 }
             }
 
-            sb.append(cnt).append("\n");
+            sb.append(ans).append("\n");
         }
 
         System.out.println(sb.toString());
     }
 
-    public static int bfs(int x, int y){
+    public static int bfs(Pos pos) {
         Queue<Pos> q = new ArrayDeque<>();
-        q.offer(new Pos(x, y));
-        visited[x][y] = true;
+        q.offer(pos);
+        visited[pos.x][pos.y] = true;
 
-        while(!q.isEmpty()){
+        while(!q.isEmpty()) {
             Pos p = q.poll();
+            int x = p.x;
+            int y = p.y;
 
-            for(int i = 0; i < 4; i++){
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                if(!isRange(nx, ny)) continue;
-
-                if(!visited[nx][ny] && map[nx][ny] == '#'){
+                if(isRange(nx, ny) && isSheep(nx, ny) && !visited[nx][ny]) {
                     visited[nx][ny] = true;
                     q.offer(new Pos(nx, ny));
                 }
@@ -77,7 +76,11 @@ public class Main {
         return 1;
     }
 
-    public static boolean isRange(int x, int y){
+    private static boolean isSheep(int nx, int ny) {
+        return map[nx][ny] == '#';
+    }
+
+    public static boolean isRange(int x, int y) {
         return x >= 0 && x < n && y >= 0 && y < m;
     }
 
