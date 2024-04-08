69ed06c3129d5ff7dff03b6559925bd015533298
0takkk <ss0takk@gmail.com>
2024-04-08 18:30:41 +0900

    [Bronze II] Title: 방 배정하기, Time: 128 ms, Memory: 14292 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.java
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -14,15 +14,14 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
 
         int[] people = new int[] {a, b, c};
-        boolean[] room = new boolean[n+1];
+        boolean[] room = new boolean[301];
         room[a] = room[b] = room[c] = true;
 
-        for(int i = 1; i <= n; i++) {
-            if(room[i]) {
-                for(int j = 0; j < 3; j++) {
-                    if(i + people[j] <= n) {
-                        room[i+people[j]] = true;
-                    }
+        for(int i = a; i <= 300; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(i >= people[j] && room[i-people[j]]) {
+                    room[i] = true;
+                    break;
                 }
             }
         }
