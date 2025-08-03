c05b493fd41ac7d7c08a58ab4f5071bcb8d4452d
0takkk <ss0takk@gmail.com>
2025-08-03 22:40:46 +0900

    [level 2] Title: 완전범죄, Time: 0.51 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    
+    final int INF = 987654321;
+    
+    public int solution(int[][] info, int n, int m) {
+        int answer = 0;
+        
+        int size = info.length;
+        int[][] dp = new int[size+1][m];
+        
+        for(int i = 0; i <= size; i++) {
+            Arrays.fill(dp[i], INF);
+        }
+        
+        dp[0][0] = 0;
+        
+        for(int i = 1; i <= size; i++) {
+            int a = info[i-1][0];
+            int b = info[i-1][1];
+            
+            for(int j = 0; j < m; j++) {
+                dp[i][j] = Math.min(dp[i][j], dp[i-1][j] + a);
+                
+                if(j + b < m) {
+                    dp[i][j + b] = Math.min(dp[i][j + b], dp[i-1][j]); 
+                }
+            }
+        }
+        
+        answer = INF;
+        for(int j = 0; j < m; j++) {
+            answer = Math.min(answer, dp[size][j]);
+        }
+        
+        return answer >= n ? -1 : answer;
+    }
+}
